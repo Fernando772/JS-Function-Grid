9c78240ce1372a040e536504403bfae1d5cb3386
fernando772 <frod791@gmail.com>
2017-02-06 15:29:33 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function setRow(row,color){
+    setColor(row,0,color);
+    setColor(row,1,color);
+    setColor(row,2,color);
+}
+
+
 //Write your function calls below this line
+setColor(3,3,"purple");
+setColor(2,2,"gray");
+setColor(3,2,"gray");
+set()
+//collum 9 , row 9 MAX
+
+setRow(5,"pink");
